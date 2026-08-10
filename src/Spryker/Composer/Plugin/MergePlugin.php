5e553904a999c4e54fe280e860987a564c9bba29
Olha Livitchuk <olha.livitchuk@spryker.com>
2026-08-10 13:29:08 +0200

    FRW-11837 Added PHP 8.5 Support

--- src/Spryker/Composer/Plugin/MergePlugin.php
+++ src/Spryker/Composer/Plugin/MergePlugin.php
@@ -230,7 +230,6 @@ class MergePlugin implements PluginInterface, EventSubscriberInterface
         $ioReflection = new \ReflectionClass($io);
 
         $inputReflection = $ioReflection->getProperty('input');
-        $inputReflection->setAccessible(true);
 
         /** @var InputInterface $input */
         $input = $inputReflection->getValue($io);
